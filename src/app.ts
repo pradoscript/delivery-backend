1e92bc094df165dc94ca107d722623df66539866
pradoscript <prado.joao06480@gmail.com>
2025-08-11 16:55:25 -0300

    feat: error handling

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,11 @@
 import express from 'express'
+import { errorHandling } from './middlewares/error-handling'
 
 const app = express()
 
 app.use(express.json())
+
+
+app.use(errorHandling)
 
 export { app }
