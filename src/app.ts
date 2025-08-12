a8ceda974ab84e23d8eadf99a13423669c0e8b9d
pradoscript <prado.joao06480@gmail.com>
2025-08-11 22:38:03 -0300

    feat: database schema and a simple route

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,11 @@
 import express from 'express'
 import { errorHandling } from './middlewares/error-handling'
+import { routes } from './routes'
 
 const app = express()
 
 app.use(express.json())
-
+app.use(routes)
 
 app.use(errorHandling)
 
