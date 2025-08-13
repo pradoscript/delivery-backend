4d5b98116d8fb25eeabde516b19753a87320a349
pradoscript <prado.joao06480@gmail.com>
2025-08-13 08:03:40 -0300

    feat: user controller

--- src/controllers/users-controller.ts
+++ src/controllers/users-controller.ts
@@ -1,8 +1,36 @@
 import { Request, Response, NextFunction } from "express"
-
+import { prisma } from "@/database/prisma"
+import { hash } from "bcrypt"
+import { z } from "zod"
+import { AppError } from "@/utils/AppError"
 class UsersController {
     async create(request: Request, response: Response, next: NextFunction) {
-        return response.status(200).json({ message: "Ok" })
+        const bodySchema = z.object({
+            name: z.string().trim().min(2),
+            email: z.string().email(),
+            password: z.string().min(6),
+        })
+        const { name, email, password } = bodySchema.parse(request.body)
+
+        const userWithEmail = await prisma.user.findFirst({ where: { email } })
+
+        if (userWithEmail) {
+            throw new AppError("User with same email already exists!")
+        }
+
+        const hashedPassword = await hash(password, 8)
+
+        const user = await prisma.user.create({
+            data: {
+                name,
+                email,
+                password: hashedPassword
+            }
+        })
+
+        const { password: _, ...userWithoutPassword } = user
+
+        return response.status(201).json({ user: userWithoutPassword })
     }
 }
 
